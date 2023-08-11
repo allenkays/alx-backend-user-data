46d112eebc9097ede19f71b1784308b391b78e49
allenkays <allenkays@tutanota.com>
2023-08-11 18:23:49 +0300

    Add base64 creds extraction from authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,14 @@ class BasicAuth(Auth):
     """
     Subclass of Auth
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Method to extract base64 encoded authorization from the header
+        """
+        if authorization_header is none or not isinstance(authorization_header, str):
+            return None
+        if authorization_header.startsWith(Basic):
+            return None
+        # base64_cred = authorization_header.split(" ")[1]
+
+        return authorization_header[len("Basic "):]
