db89b154944e512b0f73c7af7a3013c44a772f4b
allenkays <allenkays@tutanota.com>
2023-08-17 23:45:10 +0300

    Add user registration method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 This module contains methods that handle authentication
 """
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,3 +16,26 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+class Auth:
+    """
+    Class to handle all authentication
+    """
+    def register_user(self, email, password):
+        """
+        Registers a new user
+        """
+        # Check if the user already exists
+        if self._db.get_user_by_email(email) is not None:
+            raise ValueError("User {} already exists".format(email))
+
+        # Hash the password
+        hashed_password = self._hash_password(password)
+
+        # Create a new User object and save it
+        user = User(email, hashed_password
+        self._db.save_user(user)
+
+        return user
+
