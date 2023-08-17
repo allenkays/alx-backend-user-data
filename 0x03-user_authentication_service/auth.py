53914c2ab3ae8935828c2baa33666abb56873fcb
allenkays <allenkays@tutanota.com>
2023-08-17 23:26:22 +0300

    Add passwd hashing function

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains methods that handle authentication
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Generate a salted hash of the input password
+    """
+    # Generate a random salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password using the generated salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
